e760a028514ea6992c0e806c58f973e4e877098b
Aditya Narayan <writetobriankim@gmail.com>
2017-02-02 21:17:32 -0500

    "updated stat VC"

--- PolyCam/PolyCam/StatViewController.m
+++ PolyCam/PolyCam/StatViewController.m
@@ -14,10 +14,25 @@
 @end
 
 @implementation StatViewController
-
+{
+    Mediator *_mediator;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    _mediator = [Mediator sharedInstance];
+    
+    
+}
+
+-(void) viewWillAppear:(BOOL)animated{
+    
+    [super viewWillAppear:animated];
+    
+    self.overallAngerPercentage.text = [NSString stringWithFormat:@"%f", _mediator.averageOverallAnger];
+    self.overallContemptPercentage.text = [NSString stringWithFormat:@"%f", _mediator.averageOverallContempt];
+    self.overallFearPercentage.text = [NSString stringWithFormat:@"%f", _mediator.averageOverallFear];
+    self.overallSurprisePercentage.text = [NSString stringWithFormat:@"%f", _mediator.averageOverallSurprise];
     
 }
 
