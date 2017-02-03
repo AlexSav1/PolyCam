d02bbd0937754b76b237dc6c3a2f1af4274e49f1
Alex Laptop <alex@192.168.1.8>
2017-02-03 05:23:47 -0500

    added more to statVC.m

--- PolyCam/PolyCam/StatViewController.m
+++ PolyCam/PolyCam/StatViewController.m
@@ -60,19 +60,27 @@ alpha:1.0]
 {
 //    self.isScrolling = NO;
     
-    self.overallAngerPercentage.text = [NSString stringWithFormat:@"%f", _mediator.averageOverallAnger];
-    self.overallContemptPercentage.text = [NSString stringWithFormat:@"%f", _mediator.averageOverallContempt];
-    self.overallFearPercentage.text = [NSString stringWithFormat:@"%f", _mediator.averageOverallFear];
-    self.overallSurprisePercentage.text = [NSString stringWithFormat:@"%f", _mediator.averageOverallSurprise];
+    self.overallAngerPercentage.text = [NSString stringWithFormat:@"%2.2f%%", _mediator.averageOverallAnger*100];
+    self.overallContemptPercentage.text = [NSString stringWithFormat:@"%2.2f%%", _mediator.averageOverallContempt*100];
+    self.overallFearPercentage.text = [NSString stringWithFormat:@"%2.2f%%", _mediator.averageOverallFear*100];
+    self.overallSurprisePercentage.text = [NSString stringWithFormat:@"%2.2f%%", _mediator.averageOverallSurprise*100];
     
     self.overallTruthPercentage.text = [NSString stringWithFormat:@"%f", _mediator.truthfulness];
     
-    NSLog(@"ANG: %f", _mediator.averageOverallAnger);
-    NSLog(@"Con: %f", _mediator.averageOverallContempt);
-    NSLog(@"FEAR: %f", _mediator.averageOverallFear);
-    NSLog(@"Sur: %f", _mediator.averageOverallSurprise);
+    //NSLog(@"ANG: %f", _mediator.averageOverallAnger*100);
+    NSLog(@"ANG: %2.2f%%", _mediator.averageOverallAnger*100);
+    NSLog(@"Con: %2.2f%%", _mediator.averageOverallContempt*100);
+    NSLog(@"FEAR: %2.2f%%", _mediator.averageOverallFear*100);
+    NSLog(@"Sur: %2.2f%%", _mediator.averageOverallSurprise*100);
     
     NSLog(@"Truth: %f", _mediator.truthfulness);
+    
+    
+    self.overallAngerBarConstraint.constant =  _mediator.averageOverallAnger*100;
+    self.overallContemptBarConstraint.constant = _mediator.averageOverallContempt*100;
+    self.overallFearBarConstraint.constant = _mediator.averageOverallFear*100;
+    self.overallSurpriseBarConstraint.constant = _mediator.averageOverallSurprise*100;
+    
     
     [self.questionsTableView reloadData];
 }
