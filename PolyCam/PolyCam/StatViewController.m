17b88e082945347ae63825da0be336772e6a108c
Aditya Narayan <writetobriankim@gmail.com>
2017-02-02 20:33:56 -0500

    Created stat screen

--- PolyCam/PolyCam/StatViewController.m
+++ PolyCam/PolyCam/StatViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "StatViewController.h"
+#import "Mediator.h"
 
 @interface StatViewController ()
 
@@ -17,6 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
 }
 
 - (void)didReceiveMemoryWarning {
